5e7867293f58e5ae511785144652698adccf2767
juan <jcamacho249@unab.edu.co>
2025-10-27 20:45:46 -0500

    [Delete] Cleaning up unused assets from top-down template

--- Source/Kumu/KumuPlayerController.cpp
+++ Source/Kumu/KumuPlayerController.cpp
@@ -69,10 +69,6 @@ void AKumuPlayerController::OnInputStarted()
 
 void AKumuPlayerController::OnSetDestinationTriggered()
 {
-	
-	// We flag that the input is being pressed
-	FollowTime += GetWorld()->GetDeltaSeconds();
-	
 	// We look for the location in the world where the player has pressed the input
 	FHitResult Hit;
 	bool bHitSuccessful = false;
@@ -89,29 +85,14 @@ void AKumuPlayerController::OnSetDestinationTriggered()
 	if (bHitSuccessful)
 	{
 		CachedDestination = Hit.Location;
-	}
-	
-	// Move towards mouse pointer or touch
-	APawn* ControlledPawn = GetPawn();
-	if (ControlledPawn != nullptr)
-	{
-		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
-		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+		UE_LOG(LogKumu, Warning, TEXT("Input Location: %s"), *CachedDestination.ToString());
 	}
 }
 
 void AKumuPlayerController::OnSetDestinationReleased()
 {
 	UE_LOG(LogKumu, Warning, TEXT("Input Released"));
-	// If it was a short press
-	if (FollowTime <= ShortPressThreshold)
-	{
-		// We move there and spawn some particles
-		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
-		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
-	}
 
-	FollowTime = 0.f;
 }
 
 // Triggered every frame when the input is held down
