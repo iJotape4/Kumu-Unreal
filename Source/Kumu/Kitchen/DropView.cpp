661ee379ef869188de8f882155528b58b9d18c24
juan <jcamacho249@unab.edu.co>
2025-11-10 20:02:23 -0500

    [Add] DropView class

---
+++ Source/Kumu/Kitchen/DropView.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DropView.h"
+
+#include "Kumu.h"
+
+
+// Sets default values for this component's properties
+UDropView::UDropView()
+{
+	OwnerActor = GetOwner();
+}
+
+
+// Called when the game starts
+void UDropView::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDropView::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
