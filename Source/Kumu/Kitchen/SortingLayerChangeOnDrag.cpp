6fe13d6dd1ce8556224a561a63ca3082a119298c
juan <jcamacho249@unab.edu.co>
2025-11-11 19:07:13 -0500

    [Add] SortingLayerChangeOnDrag Script

---
+++ Source/Kumu/Kitchen/SortingLayerChangeOnDrag.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SortingLayerChangeOnDrag.h"
+
+
+
+// Sets default values for this component's properties
+USortingLayerChangeOnDrag::USortingLayerChangeOnDrag()
+{
+	
+}
+
+void USortingLayerChangeOnDrag::OnRegister()
+{
+	Super::OnRegister();
+	ComponentUtils::SetUpRequiredComponent<UDragView>(GetOwner(), dragView);
+}
+
+
+// Called when the game starts
+void USortingLayerChangeOnDrag::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void USortingLayerChangeOnDrag::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/Kumu/Kitchen/SortingLayerChangeOnDrag.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DragView.h"
+#include "ComponentUtils.h"
+#include "Components/ActorComponent.h"
+#include "SortingLayerChangeOnDrag.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class KUMU_API USortingLayerChangeOnDrag : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USortingLayerChangeOnDrag();
+
+	virtual void OnRegister() override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+};
